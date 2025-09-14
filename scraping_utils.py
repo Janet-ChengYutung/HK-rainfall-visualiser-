b7b4fff8f9fde300d7ea4c9e21fb868f2014542d
Janet <25036149g@connect.polyu.hk>
2025-09-14 19:04:51 +0800

    adding py files

---
+++ scraping_utils.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from lxml import html
+import json
+
+
+def get_url(url, filename):
+    if not os.path.exists(filename):
+
+        # fetch the page if it doesn't exist
+        page = requests.get(url)
+
+        # save the page to a file
+        try:
+            with open(filename, 'w', encoding='UTF8') as f:
+                f.write(page.text)
+            page = page.text
+        except Exception as e:
+            print('There is a problem saving the file:', e)
+            return None
+
+    else:
+        # if the page exists, read it from the file
+        with open(filename, 'r', encoding='UTF8') as f:
+            page = f.read() 
+            
+    return page
+
+def parse(page, mode = 'html'):
+    match mode:
+        case 'html':
+            return html.fromstring(page)
+        case 'json':
+            return json.loads(page)
